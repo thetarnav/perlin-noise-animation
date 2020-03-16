5d98c27eafa26c05a1f46dc5a65caf18c6859956
Damian Tarnawski @thetarnav <gthetarnav@gmail.com>
2020-03-16 21:50:07 +0100

    Some small changes + build for codepen

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
-const { pToVal, setCssProps, round, stayInRange, random } = require('./helpers')
+const { stayInRange } = require('./helpers')
 
-import anime from 'animejs'
+// import anime from 'animejs'
 import easing from './easing'
 import Noise from '@trinkets/noise'
 const getPerlin = Noise['@trinkets/noise'].perlin
@@ -14,10 +14,10 @@ setInterval(generateNextValue, 300)
 
 let n = 0
 function generateNextValue() {
-	let noise = getPerlin(++n/3, 0)
+	let noise = getPerlin(++n / 3, 0)
 	// noise+=.5
-	noise = stayInRange(noise, 0, .5, 'bounce')
-	noise*=2
+	noise = stayInRange(noise, 0, 0.5, 'bounce')
+	noise *= 2
 	// noise = round(noise, -100)
 
 	noise = easing.easeInOutSin(noise)
@@ -37,14 +37,13 @@ function generateNextValue() {
 		translateY: (el, i) => -valuesList[i] * (85 - 12),
 		duration: 3000,
 		easing: 'easeOutElastic(1, .8)',
-	})	
+	})
 }
-
 
 // logging perlin noise
 const perlinXes = document.querySelectorAll('.perlin p')
 perlinXes.forEach((p, i) => {
-	let noise = getPerlin(i/3, 0)
+	let noise = getPerlin(i / 3, 0)
 	// noise+=.5
 	noise = stayInRange(noise, 0, 1, 'bounce')
 	noise = easing.easeOutQuad(noise)
